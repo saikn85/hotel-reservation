00d815d270a6c6e52e7312d57ed443a937775801
saikn85 <85madmaxfury@gmail.com>
2023-09-09 21:48:16 +0530

    base setup - incomplete

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import common.MenOptions;
+import common.PatternPrinter;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        runApplication();
+    }
+
+    /**
+     * Will be the strating point in the application
+     */
+    private static void runApplication() {
+        String line = "";
+        Scanner scanner = new Scanner(System.in);
+        MenOptions userOption = MenOptions.EXIT;
+
+        try {
+            PatternPrinter.printStars();
+            PatternPrinter.printStars();
+            System.out.println("Welcome to the Hotel Reservation Application");
+            PatternPrinter.printStars();
+            PatternPrinter.printStars();
+            do {
+                System.out.println("1. Customer Menu");
+                System.out.println("2. Administrative Menu");
+                System.out.println("3. Exit");
+                System.out.print("Please select an option from the above mentioned: ");
+            } while (userOption != MenOptions.EXIT);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 }
---
+++ src/common/PatternPrinter.java
@@ -0,0 +1,12 @@
+package common;
+
+/**
+ * Helper for better string formats on Console
+ */
+public class PatternPrinter {
+    private static int Count = 20;
+
+    public static void printStars() {
+        System.out.println("*".repeat(Count));
+    }
+}
