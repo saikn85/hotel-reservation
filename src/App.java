351ccb5535fd4abc321c870d0099e81f174323ae
saikn85 <85madmaxfury@gmail.com>
2023-09-10 23:24:38 +0530

    review - 5.2

--- src/App.java
+++ src/App.java
@@ -12,7 +12,6 @@ public class App {
      * Will be the strating point in the application
      */
     private static void runApplication() {
-        Scanner scanner = new Scanner(System.in);
         MainMenuOptions userOption = MainMenuOptions.EXIT;
 
         PatternPrinter.printStars(MenuOptionsHelpers.getMainMenuOptLength());
@@ -21,52 +20,54 @@ public class App {
         PatternPrinter.printStars(MenuOptionsHelpers.getMainMenuOptLength());
         PatternPrinter.printStars(MenuOptionsHelpers.getMainMenuOptLength());
 
-        HotelMenu customerMenu = new HotelMenu();
-        AdministrativeMenu adminMenu = new AdministrativeMenu();
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        do {
-            try {
-                System.out.println();
-                System.out.println("1.\tCustomer Menu");
-                System.out.println("2.\tAdministrative Menu");
-                System.out.println("3.\tExit");
-                System.out.print(MenuOptionsHelpers.getSelectionOptionHelper());
-                switch (Integer.parseInt(scanner.next())) {
-                    case 1:
-                        userOption = customerMenu.hotelMenuOptions();
-                        break;
-                    case 2:
-                        userOption = adminMenu.adminMenuOptions();
-                        break;
-                    case 3:
-                        scanner.close();
-                        userOption = MainMenuOptions.EXIT;
-                        System.out.println("Exited");
-                        break;
-                    default:
-                        userOption = MainMenuOptions.TRYAGAIN;
-                        System.out.println();
-                        PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
-                        System.out.println();
-                        System.out.println("Invalid option specified, try again.");
-                        System.out.println();
-                        PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
-                        break;
+            HotelMenu customerMenu = new HotelMenu();
+            AdministrativeMenu adminMenu = new AdministrativeMenu();
+
+            do {
+                try {
+                    System.out.println();
+                    System.out.println("1.\tCustomer Menu");
+                    System.out.println("2.\tAdministrative Menu");
+                    System.out.println("3.\tExit");
+                    System.out.print(MenuOptionsHelpers.getSelectionOptionHelper());
+                    switch (Integer.parseInt(scanner.next())) {
+                        case 1:
+                            userOption = customerMenu.hotelMenuOptions();
+                            break;
+                        case 2:
+                            userOption = adminMenu.adminMenuOptions();
+                            break;
+                        case 3:
+                            scanner.close();
+                            userOption = MainMenuOptions.EXIT;
+                            System.out.println("Exited");
+                            break;
+                        default:
+                            userOption = MainMenuOptions.TRYAGAIN;
+                            System.out.println();
+                            PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
+                            System.out.println();
+                            System.out.println("Invalid option specified, try again.");
+                            System.out.println();
+                            PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
+                            break;
+                    }
+                } catch (NumberFormatException ex) {
+                    userOption = MainMenuOptions.TRYAGAIN;
+                    System.out.println();
+                    PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
+                    System.out.println();
+                    System.out.println("Invalid option specified, try again.");
+                    System.out.println();
+                    PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
+                } catch (Exception e) {
+                    System.err.println("Exception Occured :: " + e.getMessage() + " :: " + e.getClass());
+                    return;
                 }
-            } catch (NumberFormatException ex) {
-                userOption = MainMenuOptions.TRYAGAIN;
-                System.out.println();
-                PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
-                System.out.println();
-                System.out.println("Invalid option specified, try again.");
-                System.out.println();
-                PatternPrinter.printStars(MenuOptionsHelpers.getSelectionOptLength());
-            } catch (Exception e) {
-                System.err.println("Exception Occured :: " + e.getMessage() + " :: " + e.getClass());
-                scanner.close();
-                return;
-            }
-        } while (userOption != MainMenuOptions.EXIT);
+            } while (userOption != MainMenuOptions.EXIT);
+        }
 
         return;
     }
